540b51ff7d85caab38187ccf05447633212cb69b
La7rodectus <maxmega27@gmail.com>
2024-03-21 20:48:02 +0200

    fix

--- main.tf
+++ main.tf
@@ -41,8 +41,8 @@ resource "aws_security_group" "web_server_sg" {
 resource "aws_security_group_rule" "allow_https" {
   type              = "ingress"
   description       = "HTTPS ingress"
-  from_port         = 443
-  to_port           = 443
+  from_port         = 80
+  to_port           = 80
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.web_server_sg.id
