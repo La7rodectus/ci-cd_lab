1cc6c8e416b0b0375acbb3fb1a2530b1aea2f72b
La7rodectus <la7rodectus@gmail.com>
2024-04-25 18:53:03 +0300

    test

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "web_server" {
   vpc_security_group_ids = [aws_security_group.web_server_sg.id]
   associate_public_ip_address = true
   tags = {
-    Name = "test-connect 2"
+    Name = "test-connect"
   }
 }
 
